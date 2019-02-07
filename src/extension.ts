b1628851d45be11bd92239e676af2cddd60984ff
sajiniekavindya <sajinie.14@cse.mrt.ac.lk>
2019-02-07 15:35:15 +0530

    Intialize a vscode plugin for web based ei tooling

---
+++ src/extension.ts
@@ -0,0 +1,38 @@
+// The module 'vscode' contains the VS Code extensibility API
+// Import the module and reference it with the alias vscode in your code below
+import * as vscode from 'vscode';
+
+
+
+// this method is called when your extension is activated
+// your extension is activated the very first time the command is executed
+export function activate(context: vscode.ExtensionContext) {
+	
+
+	console.log('Congratulations, your extension "synapse-language-support" is now active!');
+
+	vscode.workspace.onDidOpenTextDocument((document)=>{
+		//parsing the document	
+		let xmlData = document.getText();
+
+		//using xmldom parser
+		var DOMParser = require('xmldom').DOMParser;
+		var doc = new DOMParser().parseFromString(xmlData,'text/xml');
+
+		//getting root element and its "xmlns" attribute
+		var rootElem = doc.documentElement;
+		let xmlns = rootElem.getAttribute('xmlns');
+
+		if(xmlns === "http://ws.apache.org/ns/synapse"){
+			console.log("This is a Synapse document");
+
+			vscode.languages.setTextDocumentLanguage(document, "SynapseXml");
+		}
+
+	});
+
+
+}
+
+// this method is called when your extension is deactivated
+export function deactivate() {}
