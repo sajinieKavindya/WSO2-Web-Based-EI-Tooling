d6a4dccb35c14e6d5704528449ca80c00f08decc
sajiniekavindya <sajinie.14@cse.mrt.ac.lk>
2019-02-07 16:26:25 +0530

    changing synapse file filtering method

--- src/extension.ts
+++ src/extension.ts
@@ -3,35 +3,44 @@
 import * as vscode from 'vscode';
 
 
-
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-	
-
 	console.log('Congratulations, your extension "synapse-language-support" is now active!');
 
 	vscode.workspace.onDidOpenTextDocument((document)=>{
 		//parsing the document	
 		let xmlData = document.getText();
 
-		//using xmldom parser
-		var DOMParser = require('xmldom').DOMParser;
-		var doc = new DOMParser().parseFromString(xmlData,'text/xml');
+		//getting filename of the opened document
+		let fileExtension = document.fileName;
 
-		//getting root element and its "xmlns" attribute
-		var rootElem = doc.documentElement;
-		let xmlns = rootElem.getAttribute('xmlns');
-
-		if(xmlns === "http://ws.apache.org/ns/synapse"){
-			console.log("This is a Synapse document");
+		//checking for '.xml' extension
+		let pattern = new RegExp("\.xml$");
+		let res = pattern.test(fileExtension);
+		
+		if (res === true){
+			let synapseNSPttern = new RegExp("xmlns=\"http:\/\/ws\.apache\.org\/ns\/synapse");
+			let response = synapseNSPttern.test(xmlData);
 
 			vscode.languages.setTextDocumentLanguage(document, "SynapseXml");
 		}
 
+		// //using xmldom parser
+		// var DOMParser = require('xmldom').DOMParser;
+		// var doc = new DOMParser().parseFromString(xmlData,'text/xml');
+
+		// //getting root element and its "xmlns" attribute
+		// var rootElem = doc.documentElement;
+		// let xmlns = rootElem.getAttribute('xmlns');
+
+		// if(xmlns === "http://ws.apache.org/ns/synapse"){
+		// 	console.log("This is a Synapse document");
+
+		// 	vscode.languages.setTextDocumentLanguage(document, "SynapseXml");
+		// }
+
 	});
-
-
 }
 
 // this method is called when your extension is deactivated
